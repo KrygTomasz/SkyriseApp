acc5cf628bb20aa5e792db07b6c24e9892472230
Krygu <>
2018-10-13 18:23:01 +0200

    Initial commit

---
+++ SkyriseApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SkyriseApp
+//
+//  Created by Krygu on 13/10/2018.
+//  Copyright © 2018 Krygu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
